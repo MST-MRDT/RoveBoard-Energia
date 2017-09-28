7f200383fdc272b406f15427f6eafe310b18017f
Drue Satterfield <drscp5@mst.edu>
2017-09-28 00:30:27 -0500

    Brought in changes from main branch

--- RoveBoardEnergia/RoveUart.h
+++ RoveBoardEnergia/RoveUart.h
@@ -1,30 +1,14 @@
 // RoveBoard.h for Energia
 // Author: Gbenga Osibodu
+// Second revisor: Drue Satterfield
 //
 
 #ifndef ROVEBOARD_TM4C1294NCPDT_ROVEBOARD_GENERICS_ROVEUART_H_
 #define ROVEBOARD_TM4C1294NCPDT_ROVEBOARD_GENERICS_ROVEUART_H_
 
 #include <stddef.h>
-#include "RoveUartTypenames.h"
 
-//settings arguments:
-//constants to set word length in transmission in bits
-#define WordLength8 0
-#define WordLength7 1
-#define WordLength6 2
-#define WordLength5 3
-
-//constants to set stop bits in transmission
-#define OneStopBit 0
-#define TwoStopBit 1
-
-//constants to set up parity bits in transmission
-#define NoParity 0
-#define EvenParity 1
-#define OddParity 2
-#define AlwaysZero 3
-#define AlwaysOne 4
+#include "RoveUartStructures.h"
 
 //sets up the specified uart to run at the specified baud rate
 //inputs: index of the uart module to run (0 for uart 0, 1 for uart 1...), baud rate in bits/second (max/min ranges are board specific)
@@ -50,11 +34,6 @@ extern roveBoard_ERROR roveUartRead(roveUART_Handle uart, void* read_buffer, siz
 //returns: Information on how the process went based on roveBoard_ERROR enum.
 //Nonblocking, so if there wasn't that amount of bytes currently in the uart's incoming data buffer it returns error
 extern roveBoard_ERROR roveUartReadNonBlocking(roveUART_Handle uart, void* read_buffer, size_t bytes_to_read);
-
-//changes the uart's output settings such as settings about its parity bit, stop bits, and word length
-//inputs: reference of a setup uart module from roveUartOpen, and arguments based on the constants defined above
-//returns: Information on how the process went based on roveBoard_ERROR enum.
-extern roveBoard_ERROR roveUartSettings(roveUART_Handle uart, unsigned int parityBits, unsigned int stopBits, unsigned int wordLength);
 
 //checks how many bytes the uart currently has in its read buffer
 //inputs: reference of a setup uart module from roveUartOpen
